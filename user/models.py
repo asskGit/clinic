9e0684dc069ea4b6592273a9a1422dc5a1774243
Anas Kaarov <anasbinkurban@gmail.com>
2024-03-18 09:05:42 +0600

    added base project

--- user/models.py
+++ user/models.py
@@ -3,9 +3,11 @@ from django.db import models
 
 
 class User(AbstractUser):
+    first_name = models.CharField("Имя", max_length=255)
+    last_name = models.CharField("Фамилия", max_length=255)
     email = models.EmailField("Почта", unique=True, blank=True, null=True)
     username = models.CharField(verbose_name="Никнейм", max_length=100, unique=True, null=True, blank=True)
-    avatar = models.ImageField("Фото", upload_to='users-avatar/', blank=True, null=True)
+    photo = models.ImageField("Фото", upload_to='user-photo/', blank=True, null=True)
     birthday = models.DateField("Дата рождения", null=True, blank=True)
     gender = models.CharField("Пол", max_length=255, choices=[('Мужской', 'Мужской'), ('Женский', 'Женский')], default='Мужской')
 
